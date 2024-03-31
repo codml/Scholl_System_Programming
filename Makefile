1201e5d0420ca4ba3f4b0ec2250d4c5ab169b962
codml <kimtawann@naver.com>
2024-04-01 02:18:08 +0900

    auto

--- Makefile
+++ Makefile
@@ -1,13 +1,11 @@
 CC = gcc
-NAME = kw2020202034_opt
-SRC = kw2020202034_opt.c
+NAME = kw2020202034_opt # executable file name
+SRC = kw2020202034_opt.c # source file name
 
 all : $(NAME)
 
-$(NAME) : $(SRC)
-	$(CC) -o $(NAME) $(SRC)
+$(NAME) : $(SRC) # compile source code
+	$(CC) -o $(NAME) $(SRC) 
 
-clean : 
+clean :  # delete executable file
 	rm -rf $(NAME)
-
-.PHONY: all clean
--- kw2020202034_opt.c
+++ kw2020202034_opt.c
@@ -1,3 +1,15 @@
+///////////////////////////////////////////////////////////////////
+// File Name    : kw2020202034.c                                 //
+// Date         : 2024/04/01                                     //
+// OS           : Ubuntu 20.04.6. LTS 64bit                      //
+        //
+// Author       : Kim Tae Wan                                    //
+// Student ID   : 2020202034                                     //
+// ------------------------------------------------------------- //
+// Title : System Programming Assignment #1-1 (ftp server)       //
+// Description : parsing arguments using getopt                  //
+///////////////////////////////////////////////////////////////////
+
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,22 +23,24 @@ int main (int argc, char **argv)
 
     while ((c = getopt(argc, argv, "abc:")) != -1)
     {
+        //////////////// actions for optional arguments ////////////////
         switch (c)
         {
         case 'a':
-            aflag++;
+            aflag++; // aflag = aflag + 1
             break;
         case 'b':
-            bflag++;
+            bflag++; // bflag = bflag + 1
             break;
         case 'c':
-            cvalue = optarg;
+            cvalue = optarg; // cvalue points to the argument(string)
             break;
         }
     }
     printf("aflag= %d, bflag= %d, cvalue= %s\n", aflag, bflag, cvalue);
  
-    for (index = optind; index < argc; index++)
+    ////////////////////print non-option arguments///////////////////
+    for (index = optind; index < argc; index++) // start from optind to argc - 1
         printf("Non-option argument %s\n", argv[index]);
     return 0;
 }
