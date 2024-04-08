bd473ca10769a115cac288e47b6318a7513f5783
codml <kimtawann@naver.com>
2024-04-08 20:07:45 +0900

    auto

--- 1-2/kw2020202034_ls.c
+++ 1-2/kw2020202034_ls.c
@@ -1,3 +1,15 @@
+////////////////////////////////////////////////////////////////////////
+// File Name    :kw2020202034_ls.c                                    //
+// Date         :2024/04/08                                           //
+// OS           :Ubuntu 20.04.6 LTS 64bits                            //
+// Author       :Kim Tae Wan                                          //
+// Student ID   :2020202034                                           //
+// ------------------------------------------------------------------ //
+// Title        :System Programming Assignment #1-2: ls               //
+// Description  :...                                                  //
+////////////////////////////////////////////////////////////////////////
+
+
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
@@ -8,28 +20,38 @@ int main(int argc, char *argv[])
 {
 	DIR		*dp;
 	struct dirent	*dirp;
+	char		*errorM;
 
 	char pathname[128];
 
-	if (argc == 1)
+	////////////argument number check//////////////
+	if (argc == 1) // read current directory
 		strcpy(pathname, ".");
-	else if (argc > 2)
+	else if (argc > 2) // get more than 1 path
 	{
-		fprintf(stderr, "Too many parameters...\n");
-		return 0;
+		fprintf(stderr, "only one directory path can be processed\n"); // print error
+		return 1;
 	}
 	else
-		strcpy(pathname, argv[1]);
-	if ((dp = opendir(pathname)) == NULL)
+		strcpy(pathname, argv[1]); // read argument directory
+
+	////////////read file names in the directory////////////
+	if ((dp = opendir(pathname)) == NULL) // fail to open directory stream
 	{
-		fprintf(stderr, "testls: cannot access '%s': %s\n", pathname, strerror(errno));
-		return 0;
+		if (errno == EACCES) // Permission denied
+			errorM = "Access denied";
+		else if (errno == ENOENT) // no such directory or name is empty string
+			errorM = "No such directory";
+		else
+			errorM = strerror(errno);
+		fprintf(stderr, "testls: cannot access '%s': %s\n", pathname, errorM);
+		return 1;
 	}
-	while (dirp = readdir(dp))
+	while (dirp = readdir(dp)) // read until meet NULL -> means read all directory entries
 	{
 		if (dirp->d_ino != 0)
-			printf("%s\n", dirp->d_name);
+			printf("%s\n", dirp->d_name); // print file name
 	}
-	closedir(dp);
+	closedir(dp); // close directory stream
 	return 0;
 }
--- header.sh
+++ header.sh
@@ -15,16 +15,16 @@ fi
 cat $1 > temp
 rm $1
 echo -e "////////////////////////////////////////////////////////////////////////" > $1
-printf "// File Name	:%-52s //\n" $1 >> $1
-printf "// Date		:%d/%02d/%02d%42s //\n" $(date +%Y) $(date +%m) $(date +%d) "" >> $1
-printf "// OS		:%-52s //\n" 'Ubuntu 20.04.6 LTS 64bits' >> $1
+printf "// File Name    :%-52s //\n" $1 >> $1
+printf "// Date         :%s/%s/%s%42s //\n" $(date +%Y) $(date +%m) $(date +%d) "" >> $1
+printf "// OS           :%-52s //\n" 'Ubuntu 20.04.6 LTS 64bits' >> $1
 #change by your name
-printf "// Author	:%-52s //\n" 'Kim Tae Wan' >> $1
+printf "// Author       :%-52s //\n" 'Kim Tae Wan' >> $1
 #change by your ID
-printf "// Student ID	:%-52d //\n" '2020202034' >> $1
+printf "// Student ID   :%-52d //\n" '2020202034' >> $1
 echo "// ------------------------------------------------------------------ //" >> $1
-printf "// Title	:System Programming Assignment #%s: %-17s//\n" $2 $3 >> $1
-printf "// Description	:%-52s //\n" '...' >> $1
+printf "// Title        :System Programming Assignment #%s: %-17s//\n" $2 $3 >> $1
+printf "// Description  :%-52s //\n" '...' >> $1
 echo -e "////////////////////////////////////////////////////////////////////////\n\n" >> $1
 
 # function header
