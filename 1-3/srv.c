2091faf411a6fb1a1f6cfb7d74a9f605b191515c
codml <kimtawann@naver.com>
2024-04-14 17:34:40 +0900

    auto

--- 1-3/srv.c
+++ 1-3/srv.c
@@ -11,7 +11,7 @@
 
 #define MAX_BUF 4096
 
-void	MtoS(mode_t st_mode, char *string);
+void	MtoS(struct stat *infor, char *pathname, char *print_buf);
 void	NLST(char *buf);
 void	LIST(char *buf);
 void	PWD(char *buf);
@@ -57,43 +57,63 @@ void	main()
 	}
 }
 
-void	MtoS(mode_t st_mode, char *str)
+void	MtoS(struct stat *infor, char *pathname, char *print_buf)
 {
-	if (S_ISREG(st_mode))
+	char	time_buf[32];
+	char	str[11];
+
+	if (S_ISREG(infor->st_mode))
 		str[0] = '-';
-	else if (S_ISDIR(st_mode))
+	else if (S_ISDIR(infor->st_mode))
 		str[0] = 'd';
-	else if (S_ISCHR(st_mode))
+	else if (S_ISCHR(infor->st_mode))
 		str[0] = 'c';
-	else if (S_ISBLK(st_mode))
+	else if (S_ISBLK(infor->st_mode))
 		str[0] = 'b';
-	else if (S_ISFIFO(st_mode))
+	else if (S_ISFIFO(infor->st_mode))
 		str[0] = 'p';
-	else if (S_ISLNK(st_mode))
+	else if (S_ISLNK(infor->st_mode))
 		str[0] = 'l';
-	else if (S_ISSOCK(st_mode))
+	else if (S_ISSOCK(infor->st_mode))
 		str[0] = 's';
-	str[1] = (st_mode & S_IRUSR) ? 'r' : '-';
-    str[2] = (st_mode & S_IWUSR) ? 'w' : '-';
-    str[3] = (st_mode & S_IXUSR) ? 'x' : '-';
-    str[4] = (st_mode & S_IRGRP) ? 'r' : '-';
-    str[5] = (st_mode & S_IWGRP) ? 'w' : '-';
-    str[6] = (st_mode & S_IXGRP) ? 'x' : '-';
-    str[7] = (st_mode & S_IROTH) ? 'r' : '-';
-    str[8] = (st_mode & S_IWOTH) ? 'w' : '-';
-    str[9] = (st_mode & S_IXOTH) ? 'x' : '-';
+	str[1] = (infor->st_mode & S_IRUSR) ? 'r' : '-';
+    str[2] = (infor->st_mode & S_IWUSR) ? 'w' : '-';
+    str[3] = (infor->st_mode & S_IXUSR) ? 'x' : '-';
+    str[4] = (infor->st_mode & S_IRGRP) ? 'r' : '-';
+    str[5] = (infor->st_mode & S_IWGRP) ? 'w' : '-';
+    str[6] = (infor->st_mode & S_IXGRP) ? 'x' : '-';
+    str[7] = (infor->st_mode & S_IROTH) ? 'r' : '-';
+    str[8] = (infor->st_mode & S_IWOTH) ? 'w' : '-';
+    str[9] = (infor->st_mode & S_IXOTH) ? 'x' : '-';
     str[10] = '\0';
+	strftime(time_buf, sizeof(time_buf), "%b %d %R",
+				localtime(&(infor->st_mtime)));
+	if (S_ISDIR(infor->st_mode))
+		snprintf(print_buf, MAX_BUF,
+		"%s %2ld %s %s %5ld %s %s/\n",
+		str, infor->st_nlink, getpwuid(infor->st_uid)->pw_name,
+		getgrgid(infor->st_gid)->gr_name, infor->st_size,
+		time_buf, pathname);
+	else
+		snprintf(print_buf, MAX_BUF,
+			"%s %2ld %s %s %5ld %s %s\n",
+			str, infor->st_nlink, getpwuid(infor->st_uid)->pw_name,
+			getgrgid(infor->st_gid)->gr_name, infor->st_size,
+			time_buf, pathname);
 }
 
 void	NLST(char *buf)
 {
 	char 			c;
 	int				idx;
+	int				start_idx;
+	int				len = 0;
 	int				aflag = 0;
 	int				lflag = 0;
 
 	char			*pathname;
 	char			*errorM;
+	char			*tmp;
 
 	DIR				*dp;
 	struct dirent	*dirp;
@@ -101,16 +121,17 @@ void	NLST(char *buf)
 
 
 	char			*split[256];
-	char			mode[11];
-	char			time_buf[32];
+	char			*filename[256];
+
 	char			print_buf[MAX_BUF];
 	opterr = 0;
 
 	strcpy(print_buf, buf);
 	for (char *ptr = strtok(print_buf, " "); ptr; ptr = strtok(NULL, " "))
-		split[idx++] = ptr;
-	split[idx] = NULL;
-	while ((c = getopt(idx, split, "al")) != -1)
+		split[len++] = ptr;
+	split[len] = NULL;
+
+	while ((c = getopt(len, split, "al")) != -1)
 	{
 		switch (c)
 		{
@@ -121,16 +142,20 @@ void	NLST(char *buf)
 			lflag++;
 			break;
 		case '?':
-			write(2, "Error: invalid option\n", 100);
+			errorM = "Error: invalid option\n";
+			write(2, errorM, strlen(errorM));
 			exit(1);
 		}
 	}
-	if (optind != idx && optind != idx - 1)
+
+	if (optind != len && optind != len - 1)
 	{
-		write(2, "Error: too many arguments\n", 100);
+		errorM = "Error: too many arguments\n";
+		write(2, errorM, strlen(errorM));
 		exit(1);
 	}
-	if (optind == idx)
+
+	if (optind == len)
 		pathname = ".";
 	else
 		pathname = split[optind];
@@ -151,16 +176,9 @@ void	NLST(char *buf)
 				write(2, print_buf, strlen(print_buf));
 				exit(1);
 			}
-			MtoS(infor.st_mode, mode);
-			strftime(time_buf, sizeof(time_buf), "%b %d %R",
-				localtime(&(infor.st_mtime)));
-			snprintf(print_buf, sizeof(print_buf),
-				"%s %2ld %s %s %6ld %s %s\n",
-				mode, infor.st_nlink, getpwuid(infor.st_uid)->pw_name,
-				getgrgid(infor.st_gid)->gr_name, infor.st_size,
-				time_buf, pathname);
+			MtoS(&infor, pathname, print_buf);
+			strcat(buf, "\n");
 			write(1, buf, strlen(buf));
-			write(1, "\n", 1);
 			write(1, print_buf, strlen(print_buf));
 			exit(0);
 		}
@@ -168,52 +186,433 @@ void	NLST(char *buf)
 		write(2, print_buf, strlen(print_buf));
 		exit(1);
 	}
-	write(1, "Not Yet\n", 100);
+	idx = 0;
+	while (dirp = readdir(dp)) // read until meet NULL -> means read all directory entries
+	{
+		if (dirp->d_ino != 0)
+			filename[idx++] = dirp->d_name;
+	}
+	
+	len = idx;
+
+	for (int i = 0; i < len - 1; i++)
+	{
+		for (int j = i + 1; j < len; j++)
+		{
+			if (strcmp(filename[i], filename[j]) > 0)
+			{
+				tmp = filename[i];
+				filename[i] = filename[j];
+				filename[j] = tmp;
+			}
+		}
+	}
+
+	idx = len;
+	for (int i = optind; i < len; i++)
+	{
+		if (filename[i][0] != '.')
+		{
+			idx = i;
+			break;
+		}
+	}
+
+	if (aflag)
+		start_idx = 0;
+	else
+		start_idx = idx;
+	
+	strcat(buf, "\n");
+	write(1, buf, strlen(buf));
+	if (lflag)
+	{
+		for (int i = start_idx; i < len; i++)
+		{
+			stat(filename[i], &infor);
+			MtoS(&infor, filename[i], print_buf);
+			write(1, print_buf, strlen(print_buf));
+		}
+	}
+	else
+	{
+		for (int i = start_idx; i < len; i++)
+		{
+			write(1, filename[i], strlen(filename[i]));
+			if ((i - start_idx) % 5 == 4)
+				write(1, "\n", 1);
+			else
+				write(1, " ", 1);
+		}
+		write(1, "\n", 1);
+	}
 	exit(0);
 }
 
 void	LIST(char *buf)
 {
+	int				idx;
+	int				len = 0;
+	int				aflag = 0;
+	int				lflag = 0;
+
+	char			*pathname;
+	char			*errorM;
+	char			*tmp;
+
+	DIR				*dp;
+	struct dirent	*dirp;
+	struct stat		infor;
+
+
+	char			*split[256];
+	char			*filename[256];
+
+	char			print_buf[MAX_BUF];
+
+	strcpy(print_buf, buf);
+	for (char *ptr = strtok(print_buf, " "); ptr; ptr = strtok(NULL, " "))
+		split[len++] = ptr;
+	split[len] = NULL;
+
+	while (getopt(len, split, "") != -1)
+	{
+		errorM = "Error: invalid option\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
+
+	if (optind != len && optind != len - 1)
+	{
+		errorM = "Error: too many arguments\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
+
+	if (optind == len)
+		pathname = ".";
+	else
+		pathname = split[optind];
+	
+	if ((dp = opendir(pathname)) == NULL)
+	{
+		if (errno == ENOTDIR)
+		{
+			if(stat(pathname, &infor) == -1)
+			{
+				if (errno == EACCES)
+					errorM = "cannot access";
+				else if (errno == ENOENT)
+					errorM = "No such file or directory";
+				else
+					errorM = strerror(errno);
+				snprintf(print_buf, sizeof(print_buf), "Error : %s\n", errorM);
+				write(2, print_buf, strlen(print_buf));
+				exit(1);
+			}
+			MtoS(&infor, pathname, print_buf);
+			strcat(buf, "\n");
+			write(1, buf, strlen(buf));
+			write(1, print_buf, strlen(print_buf));
+			exit(0);
+		}
+		snprintf(print_buf, sizeof(print_buf), "Error : %s\n", strerror(errno));
+		write(2, print_buf, strlen(print_buf));
+		exit(1);
+	}
+
+	idx = 0;
+	while (dirp = readdir(dp)) // read until meet NULL -> means read all directory entries
+	{
+		if (dirp->d_ino != 0)
+			filename[idx++] = dirp->d_name;
+	}
+	
+	len = idx;
+
+	for (int i = 0; i < len - 1; i++)
+	{
+		for (int j = i + 1; j < len; j++)
+		{
+			if (strcmp(filename[i], filename[j]) > 0)
+			{
+				tmp = filename[i];
+				filename[i] = filename[j];
+				filename[j] = tmp;
+			}
+		}
+	}
+	
+	strcat(buf, "\n");
+	write(1, buf, strlen(buf));
+	for (int i = 0; i < len; i++)
+	{
+		stat(filename[i], &infor);
+		MtoS(&infor, filename[i], print_buf);
+		write(1, print_buf, strlen(print_buf));
+	}
+
 	exit(0);
 }
 
 void	PWD(char *buf)
 {
+	int		len = 0;
+	char	*errorM;
+
+	char	*split[256];
+	char	path_buf[257];
+	char	print_buf[MAX_BUF];
+	opterr = 0;
+
+	strcpy(print_buf, buf);
+	for (char *ptr = strtok(print_buf, " "); ptr; ptr = strtok(NULL, " "))
+		split[len++] = ptr;
+	split[len] = NULL;
+
+	while (getopt(len, split, "") != -1)
+	{
+		errorM = "Error: invalid option\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
+
+	if (len > 1)
+	{
+		errorM = "Error: argument is not required\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
+
+	getcwd(path_buf, sizeof(path_buf));
+
+	strcat(buf, "\n");
+	write(1, buf, strlen(buf));
+	snprintf(print_buf, MAX_BUF, "\"%s\" is current directory\n", path_buf);
+	write(1, print_buf, strlen(print_buf));
+
 	exit(0);
 }
 
 void	CWD(char *buf)
 {
+	int		len = 0;
+	char	*errorM;
+
+	char	*split[256];
+	char	path_buf[257];
+	char	print_buf[MAX_BUF];
+	opterr = 0;
+
+	strcpy(print_buf, buf);
+	for (char *ptr = strtok(print_buf, " "); ptr; ptr = strtok(NULL, " "))
+		split[len++] = ptr;
+	split[len] = NULL;
+
+	while (getopt(len, split, "") != -1)
+	{
+		errorM = "Error: invalid option\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
+
+	if (len == 1)
+	{
+		errorM = "Error: one argument is equired\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
+
+	if (len > 2)
+	{
+		errorM = "Error: too much argument\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
+
+	if (chdir(split[1])< 0)
+	{
+		if (errno == ENOENT)
+			errorM = "directory not found";
+		else
+			errorM = strerror(errno);
+		snprintf(print_buf, MAX_BUF, "Error: %s\n", errorM);
+		write(2, print_buf, strlen(print_buf));
+		exit(1);
+	}
+	
+	getcwd(path_buf, sizeof(path_buf));
+	
+	strcat(buf, "\n");
+	write(1, buf, strlen(buf));
+	snprintf(print_buf, MAX_BUF, "\"%s\" is current directory\n", path_buf);
+	write(1, print_buf, strlen(print_buf));
+
 	exit(0);
 }
 
 void	CDUP(char *buf)
 {
+	int		len = 0;
+	char	*errorM;
+
+	char	*split[256];
+	char	path_buf[257];
+	char	print_buf[MAX_BUF];
+	opterr = 0;
+
+	strcpy(print_buf, buf);
+	for (char *ptr = strtok(print_buf, " "); ptr; ptr = strtok(NULL, " "))
+		split[len++] = ptr;
+	split[len] = NULL;
+
+	while (getopt(len, split, "") != -1)
+	{
+		errorM = "Error: invalid option\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
+
+	if (len > 1)
+	{
+		errorM = "Error: too much argument\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
+
+	if (chdir("..")< 0)
+	{
+		if (errno == ENOENT)
+			errorM = "directory not found";
+		else
+			errorM = strerror(errno);
+		snprintf(print_buf, MAX_BUF, "Error: %s\n", errorM);
+		write(2, print_buf, strlen(print_buf));
+		exit(1);
+	}
+	
+	getcwd(path_buf, sizeof(path_buf));
+	
+	strcat(buf, "\n");
+	write(1, buf, strlen(buf));
+	snprintf(print_buf, MAX_BUF, "\"%s\" is current directory\n", path_buf);
+	write(1, print_buf, strlen(print_buf));
 	exit(0);
 }
 
 void	MKD(char *buf)
 {
+	int				len = 0;
+	char			*errorM;
+
+	char			*split[256];
+	char			print_buf[MAX_BUF];
+	opterr = 0;
+	
+	strcpy(print_buf, buf);
+	for (char *ptr = strtok(print_buf, " "); ptr; ptr = strtok(NULL, " "))
+		split[len++] = ptr;
+	split[len] = NULL;
+
+	while (getopt(len, split, "") != -1)
+	{
+		errorM = "Error: invalid option\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
+	
 	exit(0);
 }
 
 void	DELE(char *buf)
 {
+	int				len = 0;
+	char			*errorM;
+
+	char			*split[256];
+	char			print_buf[MAX_BUF];
+	opterr = 0;
+	
+	strcpy(print_buf, buf);
+	for (char *ptr = strtok(print_buf, " "); ptr; ptr = strtok(NULL, " "))
+		split[len++] = ptr;
+	split[len] = NULL;
+
+	while (getopt(len, split, "") != -1)
+	{
+		errorM = "Error: invalid option\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
 	exit(0);
 }
 
 void	RMD(char *buf)
 {
+	int				len = 0;
+	char			*errorM;
+
+	char			*split[256];
+	char			print_buf[MAX_BUF];
+	opterr = 0;
+	
+	strcpy(print_buf, buf);
+	for (char *ptr = strtok(print_buf, " "); ptr; ptr = strtok(NULL, " "))
+		split[len++] = ptr;
+	split[len] = NULL;
+
+	while (getopt(len, split, "") != -1)
+	{
+		errorM = "Error: invalid option\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
 	exit(0);
 }
 
 void	RN(char *buf)
 {
+	int				len = 0;
+	char			*errorM;
+
+	char			*split[256];
+	char			print_buf[MAX_BUF];
+	opterr = 0;
+	
+	strcpy(print_buf, buf);
+	for (char *ptr = strtok(print_buf, " "); ptr; ptr = strtok(NULL, " "))
+		split[len++] = ptr;
+	split[len] = NULL;
+
+	while (getopt(len, split, "") != -1)
+	{
+		errorM = "Error: invalid option\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
 	exit(0);
 }
 
 void	QUIT(char *buf)
 {
+	int				len = 0;
+	char			*errorM;
+
+	char			*split[256];
+	char			print_buf[MAX_BUF];
+	opterr = 0;
+	
+	strcpy(print_buf, buf);
+	for (char *ptr = strtok(print_buf, " "); ptr; ptr = strtok(NULL, " "))
+		split[len++] = ptr;
+	split[len] = NULL;
+
+	while (getopt(len, split, "") != -1)
+	{
+		errorM = "Error: invalid option\n";
+		write(2, errorM, strlen(errorM));
+		exit(1);
+	}
 	exit(0);
 }
 
