444bdbb8b4d857745825a918a7eacfb11cfb5686
codml <kimtawann@naver.com>
2024-05-20 16:35:35 +0900

    auto

--- 3-1/cli.c
+++ 3-1/cli.c
@@ -1,3 +1,14 @@
+////////////////////////////////////////////////////////////////////////
+// File Name    :cli.c                                                //
+// Date         :2024/05/20                                           //
+// OS           :Ubuntu 20.04.6 LTS 64bits                            //
+// Author       :Kim Tae Wan                                          //
+// Student ID   :2020202034                                           //
+// ------------------------------------------------------------------ //
+// Title        :System Programming Assignment #3-1: server with login//
+// Description  :client - access to server and login with ID & passwd //
+////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,7 +37,11 @@ int main(int argc, char *argv[])
     }
 
     ////// make socket: domain: IPv4, type: stream(TCP) //////
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror(strerror(errno));
+		exit(1);
+	}
 
 	////// set server address structure(including information) //////
 	memset(&servaddr, 0, sizeof(servaddr));
@@ -34,71 +49,84 @@ int main(int argc, char *argv[])
 	servaddr.sin_addr.s_addr=inet_addr(argv[1]);
 	servaddr.sin_port=htons(atoi(argv[2]));
 
+	///// connect to server + error handling /////
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
 	{
 		perror(strerror(errno));
 		exit(1);
 	}
 
-    log_in(sockfd);
-    close(sockfd);
+    log_in(sockfd); /// check rejection & acception, read ID & passwd and send them, then receive response ///
+    close(sockfd); /// close connection ///
     return 0;
 }
+
+////////////////////////////////////////////////////////////////////////
+// log_in                                                             //
+// ================================================================== //
+// Input: int -> socket descriptor that is used for server connection //
+//                                                                    //
+// Output: none                                                       //
+//                                                                    //
+// Purpose: check connection to server, send ID & passwd and receive  //
+//				answer from server                                    //
+////////////////////////////////////////////////////////////////////////
 
 void log_in(int sockfd)
 {
     int n;
     char user[MAX_BUF], *passwd, buf[MAX_BUF];
 
+	// read REJECTION or ACCEPTED -> ip access //
     if ((n = read(sockfd, buf, MAX_BUF)) <= 0)
 	{
 		perror("Read error\n");
 		exit(1);
 	}
     buf[n] = '\0';
-    if (!strcmp(buf, "REJECTION"))
+    if (!strcmp(buf, "REJECTION")) // rejected from server
     {
 		printf("** Connection refused ""\n");
 		close(sockfd);
 		exit(1);
 	}
-	else
+	else // receive ACCEPTED
 		printf("** It is connected to Server **\n");
 
-    while (1)
+    while (1) // read ID & passwd from STDIN_FILENO and send to server, then receive OK, FAIL, or DISCONNECTION //
     {
 		write(STDOUT_FILENO, "Input ID : ", strlen("Input ID : "));
-		if ((n = read(STDIN_FILENO, buf, MAX_BUF)) <= 0)
+		if ((n = read(STDIN_FILENO, buf, MAX_BUF)) <= 0) // if read failed, exit
 			exit(1);
-		buf[n - 1] = '\0';
-		if (write(sockfd, buf, MAX_BUF) <= 0)
+		buf[n - 1] = '\0'; // store input except line feed('\n')
+		if (write(sockfd, buf, MAX_BUF) <= 0) // write ID to server
 			exit(0);
 
-		strcpy(user, buf);
+		strcpy(user, buf); // store user name(ID) to print result
 
-		passwd = getpass("Input passwd : ");
-		if (write(sockfd, passwd, MAX_BUF) <= 0)
+		passwd = getpass("Input passwd : "); // receive passwd encrypted
+		if (write(sockfd, passwd, MAX_BUF) <= 0) // write to server
 			exit(1);
 
-		if ((n = read(sockfd, buf, MAX_BUF)) <= 0)
+		if ((n = read(sockfd, buf, MAX_BUF)) <= 0) // receive result from server
 			exit(1);
 		buf[n] = '\0';
-        if (!strcmp(buf, "OK"))
+        if (!strcmp(buf, "OK")) // ID, passwd arrived well
         {
-            if ((n = read(sockfd, buf, MAX_BUF)) <= 0)
+            if ((n = read(sockfd, buf, MAX_BUF)) <= 0) // read OK, or FAIL, or DISCONNECTION
 				exit(1);
             buf[n] = '\0';
 
-            if (!strcmp(buf, "OK"))
+            if (!strcmp(buf, "OK")) // login success
 			{
 				printf("** User '[%s]' logged in **\n", user);
 				break;
 			}	
-            else if (!strcmp(buf, "FAIL"))
+            else if (!strcmp(buf, "FAIL")) // fail less than 3 times
 				printf("** Log-in failed **\n");
             else
 			{
-				printf("** Connection closed **\n");
+				printf("** Connection closed **\n"); // failures during 3 times
 				break;
 			}
 				
--- 3-1/srv.c
+++ 3-1/srv.c
@@ -1,3 +1,14 @@
+////////////////////////////////////////////////////////////////////////
+// File Name    :srv.c                                                //
+// Date         :2024/05/20                                           //
+// OS           :Ubuntu 20.04.6 LTS 64bits                            //
+// Author       :Kim Tae Wan                                          //
+// Student ID   :2020202034                                           //
+// ------------------------------------------------------------------ //
+// Title        :System Programming Assignment #3-1: server with login//
+// Description  :server - accept or reject client, and check login    //
+////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +18,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pwd.h>
+#include <errno.h>
 
 #define MAX_BUF 20
 
@@ -27,7 +39,11 @@ int main(int argc, char *argv[])
     }
 
     ///// make socket for server //////
-    listenfd = socket(PF_INET, SOCK_STREAM, 0);
+    if ((listenfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror(strerror(errno));
+		exit(1);
+	}
 
     ////// set server address for bind //////
     memset(&servaddr, 0, sizeof(servaddr));
@@ -36,45 +52,54 @@ int main(int argc, char *argv[])
     servaddr.sin_port=htons(atoi(argv[1]));
 
     ///// bind server address with server socket /////
-    bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+	{
+		perror(strerror(errno));
+		exit(1);
+	}
 
-    listen(listenfd, 5);
+    listen(listenfd, 5); // listen queue size: 5
     while (1)
     {
         int clilen = sizeof(cliaddr);
-        connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
-		printf("** Client is connected **\n");
+        if ((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen)) < 0) /// accept client's connection ///
+		{
+			perror(strerror(errno));
+			exit(1);
+		}
+		printf("** Client is connected **\n"); // success to connect
 		printf(" - IP: %s\n - Port: %d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
 
-		if((fp_checkIP = fopen("access.txt", "r")) == NULL)
+		if((fp_checkIP = fopen("access.txt", "r")) == NULL) // open access.txt file and check error
 			exit(1);
 		
-		char buf[MAX_BUF];
+		char buf[MAX_BUF]; // store a line from 'access.txt'
 		char cli_ip[MAX_BUF];
-		strcpy(cli_ip, inet_ntoa(cliaddr.sin_addr));
-		char *cli_ips[5];
+		strcpy(cli_ip, inet_ntoa(cliaddr.sin_addr)); // store client ip to cli_ip
+
+		char *cli_ips[5]; // split client ip address by '.'
 		cli_ips[0] = strtok(cli_ip, ".");
 		for (int i = 1; (cli_ips[i] = strtok(NULL, ".")) != NULL; i++);
 		
-		char *ptr, *tmp;
+		char *ptr, *tmp; // ptr -> check incorrect ip address, tmp -> splitted buf
 		while ((ptr = fgets(buf, MAX_BUF, fp_checkIP)) != NULL)
 		{
 			tmp = strtok(buf, ".");
-			if (strcmp(tmp, cli_ips[0]) && strcmp(tmp, "*"))
+			if (!tmp || (strcmp(tmp, cli_ips[0]) && strcmp(tmp, "*"))) // first Byte dismatch
 				continue;
 			tmp = strtok(NULL, ".");
-			if (strcmp(tmp, cli_ips[1]) && strcmp(tmp, "*"))
+			if (!tmp || (strcmp(tmp, cli_ips[1]) && strcmp(tmp, "*"))) // second Byte dismatch
 				continue;
 			tmp = strtok(NULL, ".");
-			if (strcmp(tmp, cli_ips[2]) && strcmp(tmp, "*"))
+			if (!tmp || (strcmp(tmp, cli_ips[2]) && strcmp(tmp, "*"))) // third Byte dismatch
 				continue;
 			tmp = strtok(NULL, ".");
-			if (strcmp(tmp, cli_ips[3]) && strcmp(tmp, "*"))
+			if (!tmp || (strcmp(tmp, cli_ips[3]) && strcmp(tmp, "*"))) // last Byte dismatch
 				continue;
-			break;
+			break; // if match with ip in 'access.txt', stop compare
 		}
-		fclose(fp_checkIP);
-		if (!ptr)
+		fclose(fp_checkIP); // no longer use
+		if (!ptr) // escape while() because of no matching
 		{
 			printf("** It is NOT authenticated client **\n");
 			write(connfd, "REJECTION", MAX_BUF);
@@ -82,18 +107,29 @@ int main(int argc, char *argv[])
 			continue;
 		}
 		else
-			write(connfd, "ACCEPTED", MAX_BUF);
+			write(connfd, "ACCEPTED", MAX_BUF); // match
 
-        if (log_auth(connfd) == 0)
+        if (log_auth(connfd) == 0) // if login failed
         {
             printf("** Fail to log-in **\n");
             close(connfd);
             continue;
         }
-        printf("** Success to log-in **\n");
+        printf("** Success to log-in **\n"); // success to login
         close(connfd);
     }
 }
+
+////////////////////////////////////////////////////////////////////////
+// log_auth                                                           //
+// ================================================================== //
+// Input: int -> socket descriptor that is used for client connection //
+//                                                                    //
+// Output: int -> 0: failed to login                                  //
+//                1: success to login                                 //
+// Purpose: compare user name(ID), passwd read from client with       //
+//	        ID & passwd in the file 'passwd'                          //
+////////////////////////////////////////////////////////////////////////
 
 int log_auth(int connfd)
 {
@@ -107,44 +143,55 @@ int log_auth(int connfd)
 		user[n] = '\0';
 		if ((n = read(connfd, passwd, MAX_BUF)) <= 0)
 			return 0;
-		passwd[n] = '\0';
-        write(connfd, "OK", MAX_BUF);
+		passwd[n] = '\0'; // read user name and passwd from client
+        write(connfd, "OK", MAX_BUF); // read well from client
 
-		printf("** User is trying to log-in (%d/3) **\n", count);
+		printf("** User is trying to log-in (%d/3) **\n", count); // print login try count
 
-        if ((n = user_match(user, passwd)) == 1)
+        if ((n = user_match(user, passwd)) == 1) // if user_match succeed, write OK to client
 		{
 			write(connfd, "OK", MAX_BUF);
-			return 1;
+			break;
 		}
-        else if (n == 0)
+        else if (n == 0) // failed to login
         {
-			printf("** Log-in failed **\n");
-            if (count >= 3)
+			printf("** Log-in failed **\n"); // print failures
+            if (count >= 3) // if failed more than 3 times, stop login
             {
 				write(connfd, "DISCONNECTION", MAX_BUF);
 				return 0;
             }
-            write(connfd, "FAIL", MAX_BUF);
+            write(connfd, "FAIL", MAX_BUF); // failures less than 3 times, send 'FAIL' to client 
             count++;
             continue;
         }
     }
-    return 1;
+    return 1; // return success
 }
+
+////////////////////////////////////////////////////////////////////////
+// user_match                                                         //
+// ================================================================== //
+// Input: char * -> user name                                         //
+//		  char * -> passwd                                            //
+//                                                                    //
+// Output: int -> 0 : there's no ID & passwd in 'passwd'              //
+//                1 : ID & passwd exist in 'passwd'                   //
+// Purpose: compare ID & passwd(parameter) with ID & passwd in passwd //                       
+////////////////////////////////////////////////////////////////////////
 
 int user_match(char *user, char *passwd)
 {
     FILE *fp;
     struct passwd *pw;
 
-    fp = fopen("passwd", "r");
+    fp = fopen("passwd", "r"); // open passwd file stream for read
 
-	while ((pw = fgetpwent(fp)) != NULL)
+	while ((pw = fgetpwent(fp)) != NULL) // parsing passwd line(ID:passwd:uid:gid:...) and store to struct passwd
 	{
-		if (!strcmp(user, pw->pw_name) && !strcmp(passwd, pw->pw_passwd))
-			return 1;
+		if (!strcmp(user, pw->pw_name) && !strcmp(passwd, pw->pw_passwd)) // correspond with oasswd
+			return 1; // success
 	}
-	fclose(fp);
+	fclose(fp); // close file stream
 	return 0;
 }
